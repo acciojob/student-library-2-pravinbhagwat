0fff091862a0e87fb8f01bd1d4a8355ea185608d
pravinbhagwat <pravinbhagwat150@gmail.com>
2023-01-17 14:11:25 +0530

    Build 3

--- src/main/java/com/driver/controller/BookController.java
+++ src/main/java/com/driver/controller/BookController.java
@@ -17,14 +17,14 @@ public class BookController {
     @Autowired
     BookService bookService;
     //Write createBook API with required annotations
-    @PostMapping
+    @PostMapping("/")
     public ResponseEntity createBook(@RequestBody Book book) {
         bookService.createBook(book);
         return new ResponseEntity<>("Book is added successfully", HttpStatus.CREATED);
     }
 
     //Add required annotations
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity getBooks(@RequestParam(value = "genre", required = false) String genre,
                                    @RequestParam(value = "available", required = false, defaultValue = "false") boolean available,
                                    @RequestParam(value = "author", required = false) String author){
